29401394cb276c639509506d342eda6f38f4005c
Velvet Pugh <48695329+vnpugh@users.noreply.github.com>
2023-09-28 14:24:35 -0500

    Commiting Files

---
+++ 09.28.23 (Angular Intro)/exercise-one/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'exercise-one';
+  name: string = "Velvet";
+
+  nameChange(newName: string) {
+    this.name = newName;
+  }
+}
